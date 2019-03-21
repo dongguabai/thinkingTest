5971a8902da413320ca4820fa8a2e039acb1c623
dongguabai <weiminbronn@163.com>
2019-03-21 10:30:55 +0800

    add testing module

---
+++ testing/src/main/java/dongguabai/demo/testing/Test1.java
@@ -0,0 +1,16 @@
+package dongguabai.demo.testing;
+
+/**
+ * @author dongguabai
+ * @date 2019-03-20 23:39
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println();
+
+    }
+}
